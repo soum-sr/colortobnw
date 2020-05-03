062af0355f3f79a2acbf193122a1ead7a676cf25
soum-sr <soum1071@gmail.com>
2020-05-04 01:14:47 +0530

    convert color to bnw

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import os
+from PIL import Image
 from flask import Flask 
 from flask import request, jsonify,render_template
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def process_image(location):
+	image_file = Image.open(location)
+	image_file = image_file.convert('1')
+	target_path = os.path.join(APP_ROOT, 'outputs/')
+	print('TARGE: ', target_path)
+
+	if not os.path.isdir(target_path):
+		os.mkdir(target_path)
+	dest = '/'.join([target_path, 'out.png'])
+	image_file.save(dest)
+	return dest
 
 @app.route('/')
 def hello():
@@ -26,8 +39,8 @@ def upload():
 		destination = '/'.join([target, filename])
 		print('Destination: ', destination)
 		file.save(destination)
-
-	return render_template('complete.html')
+	processed_image = process_image(destination)
+	return render_template('complete.html',process_image)
 
 
 
