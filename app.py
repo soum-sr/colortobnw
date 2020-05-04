13f95539700e03f67188f647e36ba45b77b6ef6d
soum-sr <soum1071@gmail.com>
2020-05-04 14:24:27 +0530

    upload and bnw

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import os
 from PIL import Image
 from flask import Flask 
-from flask import request, jsonify,render_template
+from flask import request, jsonify,render_template,send_from_directory
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-def process_image(location):
-	image_file = Image.open(location)
+def process_image(filename):
+	img_location = './images/' + filename
+	image_file = Image.open(img_location)
 	image_file = image_file.convert('1')
-	target_path = os.path.join(APP_ROOT, 'outputs/')
-	print('TARGE: ', target_path)
-
+	target_path = APP_ROOT + '/' + 'outputs'
+	print('TARGET: ', target_path)
+	out_filename = filename.replace('.','_')
+	out_filename = out_filename + '_out.png'
 	if not os.path.isdir(target_path):
 		os.mkdir(target_path)
-	dest = '/'.join([target_path, 'out.png'])
+
+	dest = '/'.join([target_path,out_filename])
 	image_file.save(dest)
-	return dest
+	print("OUTPUT IMAGE: ", dest)
+	return 
 
 @app.route('/')
 def hello():
@@ -31,20 +35,15 @@ def upload():
 	if not os.path.isdir(target):
 		os.mkdir(target)
 
-	filenames = request.files.getlist('file')
-
-	for file in filenames:
-		print("FILE: ", file)
-		filename = file.filename 
-		destination = '/'.join([target, filename])
-		print('Destination: ', destination)
-		file.save(destination)
-	processed_image = process_image(destination)
-	return render_template('complete.html',process_image)
-
-
-
-
+	f = request.files['file']
+	filename = f.filename 
+	destination = target+filename 
+	f.save(destination)
+	processed_image = process_image(filename)
+	print("Destination: ", destination)
+	## This to directly download the processed image
+	# return send_from_directory("outputs", processed_image, as_attachment=True)
+	return render_template('complete.html',out=filename)
 
 
 
